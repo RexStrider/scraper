9d79a0c0d4a6c7c0667c7870389eb9a84dfb3f99
Odysseus <arwaterman@gmail.com>
2019-04-21 18:37:41 -0700

    updating home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var articleController = require('../controllers/article');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', articleController.getArticles);
 
 module.exports = router;
